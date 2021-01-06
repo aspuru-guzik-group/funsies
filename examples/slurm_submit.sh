c4ce01e64043a933808030cc9ca41672af6be0c0
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-01-06 07:23:51 -0500

    added slurm submission example

---
+++ examples/slurm_submit.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+#SBATCH --cpus-per-task 20
+#SBATCH --ntasks 8
+#SBATCH --time=00:30:00
+#SBATCH --job-name NAME
+
+# environemnt loading etc
+module load NiaEnv
+module load conda3
+source activate madness-spectra
+export PATH=$HOME/xtb/bin:$PATH # xtb on path
+export PATH=$HOME/madness-spectra/shell:$PATH # xtb on path
+
+# number of workers
+let "worker_num=(${SLURM_NTASKS})"
+
+# redis server info
+suffix='6379'
+server_node=`hostname`:$suffix
+export server_node
+
+# run redis server (here we are *not* using a dedicated node, so memory could
+# be an issue. If that's the case, use --exclude and srun.)
+redis-server --protected-mode no &
+sleep 3
+# TODO: unprotected mode is kind of risky
+
+# run the funsies script
+python3 SCRIPT_NAME.py ${server_node}
+sleep 3
+
+# now we run the workers
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export MKL_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+# start workers in burst mode: work until no work is present, then quit
+srun --ntasks=${worker_num} --cpus-per-task=${SLURM_CPUS_PER_TASK}\
+     rq worker --url redis://${server_node}
