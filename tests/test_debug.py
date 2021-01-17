880c47b8194e8dfb4b3bd4bf832cd193ec49d374
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-01-17 04:10:35 -0500

    added debugging routine

---
+++ tests/test_debug.py
@@ -0,0 +1,43 @@
+"""Test debugging functions."""
+import tempfile
+import os.path
+
+# external
+from fakeredis import FakeStrictRedis as Redis
+
+# module
+from funsies import run_op, shell
+from funsies.debug import debug_shell
+
+
+def test_shell_run() -> None:
+    """Test run on a shell command."""
+    db = Redis()
+    cmd = shell("cat file1", inp={"file1": b"bla bla"}, out=["bla"], connection=db)
+    _ = run_op(db, cmd.hash)
+
+    with tempfile.TemporaryDirectory() as d:
+        debug_shell(cmd, d, connection=db)
+        n = os.listdir(d)
+        assert "stdout0" in n
+        assert "stderr0" in n
+        assert "input_files" in n
+        assert "output_files" in n
+
+        with open(os.path.join(d, "error.log"), "r") as f:
+            assert "MissingOutput" in f.read()
+
+
+def test_shell_norun() -> None:
+    """Test run on a shell command that didn't run."""
+    db = Redis()
+    cmd = shell("cat file1", inp={"file1": b"bla bla"}, out=["bla"], connection=db)
+
+    with tempfile.TemporaryDirectory() as d:
+        debug_shell(cmd, d, connection=db)
+        n = os.listdir(d)
+        assert "input_files" in n
+        assert "output_files" in n
+
+        with open(os.path.join(d, "error.log"), "r") as f:
+            assert "NotFound" in f.read()
