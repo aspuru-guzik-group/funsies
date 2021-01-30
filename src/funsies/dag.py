defa3dd670bd5a38f65144aeea65a151f99cfe64
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-01-29 19:48:04 -0500

    Previously introduced rapid evaluation was a terrible idea

--- src/funsies/dag.py
+++ src/funsies/dag.py
@@ -12,7 +12,7 @@ from ._graph import Artefact, get_artefact, get_op, get_op_options, Operation
 from .constants import DAG_STORE, hash_t
 from .context import get_db
 from .logging import logger
-from .run import is_it_cached, run_op, RunStatus
+from .run import run_op, RunStatus
 from .ui import ShellOutput
 
 
@@ -81,11 +81,15 @@ def build_dag(db: Redis, address: hash_t) -> Optional[str]:  # noqa:C901
     while len(queue) != 0:
         curr = queue.pop()
 
-        if is_it_cached(db, curr):
-            # We don't need to run this because all of its outputs are cached
-            # anyway.
-            logger.debug(f"operation {curr.hash} is cached, keeping off dag.")
-            continue
+        # This is a bad idea because two dags could end in different places
+        # but start from the same initial point. if that's the case and we
+        # preemptively remove that initial point, the other dag wont get run!
+
+        # if is_it_cached(db, curr):
+        #     # We don't need to run this because all of its outputs are cached
+        #     # anyway.
+        #     logger.debug(f"operation {curr.hash} is cached, keeping off dag.")
+        #     continue
 
         # no dependency -> add as root
         if len(curr.inp) == 0:
@@ -145,8 +149,6 @@ def execute(
 
     # get redis
     db = get_db(connection)
-    # TODO FIX QUEUE ARGS
-    queue = Queue()
 
     # make dag
     build_dag(db, dag_of)
