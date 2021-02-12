6e01110a9b964d4673b62183f82bc55526dfd112
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-02-12 14:03:04 -0500

    added integration data for v0.2 release

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,9 +15,11 @@ from funsies import (
     morph,
     put,
     reduce,
+    Result,
     shell,
     tag,
     take,
+    utils,
     wait_for,
 )
 
@@ -34,8 +36,8 @@ make_reference = False
 ref_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "reference_data")
 
 
-@pytest.mark.parametrize("reference", ["0.1"])
-@pytest.mark.parametrize("nworkers", [1, 2, 3])
+@pytest.mark.parametrize("reference", ["0.1", "0.2"])
+@pytest.mark.parametrize("nworkers", [1, 2, 8])
 def test_integration(reference: str, nworkers: int) -> None:
     """Test full integration."""
     # make a temp file and copy reference database
@@ -43,6 +45,9 @@ def test_integration(reference: str, nworkers: int) -> None:
     if not make_reference:
         shutil.copy(os.path.join(ref_dir, reference, "appendonly.aof"), dir)
     shutil.copy(os.path.join(ref_dir, "redis.conf"), dir)
+
+    # Dictionary for test data
+    test_data = {}
 
     # Start funsie script
     with ManagedFun(nworkers=nworkers, directory=dir):
@@ -64,31 +69,61 @@ def test_integration(reference: str, nworkers: int) -> None:
         estdout, merge = mapping(
             lambda x, y: (y, x), merge, echo.stdouts[1], noutputs=2
         )
+        test_data["test1"] = merge
 
-        execute(estdout)
-        wait_for(merge, timeout=10.0)
-        wait_for(estdout, timeout=10.0)
+        # features from 0.2 onward
+        if reference == "0.2":
+            # Added features:
+            # - concat
+            # - errors and error handling
+            # - lax and strict funsies
+            # - partial hash calls
 
-        # wait till completed
+            def raises(inp: bytes) -> bytes:
+                raise RuntimeError("an error was raised")
+
+            def error_count(*inp: Result[bytes]) -> bytes:
+                out = utils.match_results(inp, lambda x: 0, lambda x: 1)
+                return str(sum(out)).encode()
+
+            err = morph(raises, dat)
+            count = reduce(
+                error_count, dat, dat, err, dat, err, err, echo.stdouts[0], strict=False
+            )
+            cat = utils.concat(estdout, dat, err, count, echo.stdouts[1], strict=False)
+            test_data["test2"] = cat
+
+        # basic functionality
+        execute(step1)
+        wait_for(step1, timeout=10.0)
+        execute(step2)
+        wait_for(step2, timeout=10.0)
         assert take(step1) == b"BLA BLA"
         assert take(step2.stdout) == b"BLA BLAbla bla"
 
         if make_reference:
-            with open(os.path.join(ref_dir, reference, "test1"), "wb") as f:
-                out = take(merge)
-                assert out is not None
-                f.write(out)
+            folder = os.path.join(ref_dir, reference)
+            os.makedirs(folder, exist_ok=True)
+
+            for name, artefact in test_data.items():
+                with open(os.path.join(folder, name), "wb") as f:
+                    execute(artefact)
+                    wait_for(artefact, 10.0)
+                    out = take(artefact)
+                    f.write(out)
 
             shutil.copy(
                 os.path.join(dir, "appendonly.aof"),
-                os.path.join(ref_dir, reference, "appendonly.aof"),
+                os.path.join(folder, "appendonly.aof"),
             )
         else:
             # Test against reference dbs
-            with open(os.path.join(ref_dir, reference, "test1"), "rb") as f:
-                data = f.read()
+            for name, artefact in test_data.items():
+                execute(artefact)
+                wait_for(artefact, 10.0)
+                with open(os.path.join(ref_dir, reference, name), "rb") as f:
+                    data = f.read()
 
-            print(take(merge))
-            assert take(merge) == data
+                assert take(artefact) == data
 
     shutil.rmtree(dir)
