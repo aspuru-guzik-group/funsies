47b06a9d028eeb681b93a690716a751de3d6f56d
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-02-16 14:49:16 -0500

    retooled example for GM

--- examples/chem.py
+++ examples/chem.py
@@ -1,14 +1,31 @@
 """A quantum chemistry example."""
-# module
-from funsies import execute, Fun, options, reduce, shell, take, wait_for
-from funsies.utils import concat
+# funsies imports
+from funsies import execute, Fun, reduce, shell
+from funsies.utils import concat, stop_if
 
 # To run this example, you will need openbabel and xtb installed and on path
 # on all worker nodes.
 
+# This is the routine that outputs the HOMO-LUMO gap out of the xtb output.
+def get_gap(smi: bytes, xtbout: bytes) -> bytes:
+    """Take HOMO-LUMO gap in inp, and output it to out."""
+    for line in xtbout.decode().splitlines():
+        # we are looking for this line
+        # | HOMO-LUMO GAP               1.390012170128 eV   |
+        if "HOMO-LUMO GAP" in line:
+            gap = float(line.strip()[18:-7].strip())
+            break
+    else:
+        raise RuntimeError("HOMO-LUMO gap was not found!")
+
+    # output is a csv row
+    output = f"{smi.decode()},{gap}\n"
+    return output.encode()
+
+
 # By default, the Fun context manager connects to a Redis instance on
 # localhost, but a different redis instance can be passed to it.
-with Fun(defaults=options(timeout=60.0)):
+with Fun():
     # Start of computational workflow
     # -------------------------------
     # Our task is to take a bunch of molecules, given as SMILES, make conformers
@@ -19,68 +36,46 @@ with Fun(defaults=options(timeout=60.0)):
     smiles = [
         r"CC\C=C/C\C=C/C\C=C/CCCCCCCC(O)=O",
         r"CCN(CC)CC1=C(O)C=CC(NC2=C3C=CC(Cl)=CC3=NC=C2)=C1",
-        r"NCCCNCCSP(O)(O)=O",
-        r"NC12CC3CC(CC(C3)C1)C2",
-        r"CC(C)NCC(O)COC1=CC=C(CCOCC2CC2)C=C1",
-        r"CCC(C)C1(CC)C(=O)NC(=O)NC1=O",
-        r"CCN1N=C(C(O)=O)C(=O)C2=CC3=C(OCO3)C=C12",
+        # next smiles is invalid:
+        r"CCC(C3)C1(CC)C(=O)NC(=O)NC1=O",
         r"CN(C)CCCC1(OCC2=C1C=CC(=C2)C#N)C1=CC=C(F)C=C1",
-        r"NC1=CC=NC=C1",
-        r"NC(=N)N1CCC2=CC=CC=C2C1",
-        r"CCN(CC)CCOC(=O)C1(CCCCC1)C1CCCCC1",
-        r"FC(F)OC(F)(F)C(F)Cl",
-        r"CCNC1C2CCC(C2)C1C1=CC=CC=C1",
-        r"FC1=CNC(=O)NC1=O",
+        r"NC12CC3CC(CC(C3)C1)C2",
     ]
-
-    # # This is the routine that outputs the HOMO-LUMO gap out of the xtb output.
-    def get_gap(smi: bytes, xtbout: bytes) -> bytes:
-        """Take HOMO-LUMO gap in inp, and output it to out."""
-        for line in xtbout.decode().splitlines():
-            # we are looking for this line
-            # | HOMO-LUMO GAP               1.390012170128 eV   |
-            if "HOMO-LUMO GAP" in line:
-                gap = float(line.strip()[18:-7].strip())
-                break
-        else:
-            raise RuntimeError("HOMO-LUMO gap was not found!")
-
-        # output is a csv row
-        output = f"{smi.decode()},{gap}\n"
-        return output.encode()
 
     # We start by running obabel to transform those from SMILES to 3d conformers.
     outputs = []
-    for _, smi in enumerate(smiles):
-        t1 = shell(
-            "obabel in.smi --addh --gen3d -O init.xyz",
-            inp={"in.smi": smi},
-            out=["init.xyz"],
+    for smi in smiles:
+        make_3d = shell(
+            "obabel initial.smi --addh --gen3d --ff uff --minimize -O conf0.xyz",
+            inp={"initial.smi": smi},
+            out=["conf0.xyz"],
         )
         # t1 outputs can already be used as inputs in other jobs, even if the task
         # is not yet completed, because they are not actual values: they are
         # instead pointers to file on the redis server.
 
+        # first we make sure that output is not empty, meaning that obabel crashed
+        geom = stop_if(lambda x: len(x.strip()) == 0, make_3d.out["conf0.xyz"])
+
         # we use the output init.xyz from t1 as the input to xtb.
-        t2 = shell(
+        optimize = shell(
             "xtb init.xyz --opt --parallel 1",
-            inp={"init.xyz": t1.out["init.xyz"]},
-            out=["xtbopt.xyz"],
+            inp={"init.xyz": geom},
+            out=["xtbopt.xyz", ".xtboptok"],
+            env={"OMP_NUM_THREADS": "1"},
         )
 
-        # Now the HOMO-LUMO gap is in the xtb std output. To get it, we will use a
-        # python IO transform on the xtb stdout,
-        xtb_out = t2.stdout
+        # Now the HOMO-LUMO gap is in the xtb std output. To get it, we will
+        # use a python IO transform on the xtb stdout. We use get_gap defined
+        # above to do the transformation. We also add the SMILES string so
+        # that we can keep track of molecules.
+        outputs += [reduce(get_gap, smi, optimize.stdout)]
 
-        # We use get_gap defined above to do the transformation. We also add the
-        # SMILES string so that we can keep track of molecules.
-        outputs += [reduce(get_gap, t1.inp["in.smi"], xtb_out)]
+    # Final reduction
+    tr = concat(*outputs, join="", strict=False)
 
-    # Final transformer that joins all the outputs.
-    tr = concat(*outputs, join="")
-
-    # Execute and wait for results
+    # execute
     execute(tr)
-    wait_for(tr)
-    out = take(tr)
-    print(out.decode().rstrip() + "\n")
+
+    # print out the hash of interest
+    print(f"results can be found at {tr.hash}")
