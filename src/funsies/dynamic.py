7fdb5e18e1835fc6a81bf24778122c67addb4b1a
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-02-22 15:51:15 -0500

    map reduce is a bit convoluted, but getting there

--- src/funsies/dynamic.py
+++ src/funsies/dynamic.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 import time
 from typing import (
     Callable,
+    Dict,
     Iterable,
     Mapping,
     Optional,
+    Sequence,
     overload,
     Union,
 )
@@ -37,7 +39,7 @@ from ._graph import (
 from ._logging import logger
 from ._pyfunc import python_funsie
 from ._run import is_it_cached
-from ._shell import shell_funsie, ShellOutput
+from ._subdag import subdag_funsie
 from ._short_hash import shorten_hash
 from .config import Options
 from .errors import Error, Result, unwrap
@@ -45,36 +47,42 @@ from .ui import put
 
 # Types
 _INP_FILES = Optional[Mapping[_AnyPath, Union[Artefact, str, bytes]]]
-_OUT_FILES = Optional[Iterable[_AnyPath]]
+_OUT_FILES = Iterable[_AnyPath]
 
 
 # --------------------------------------------------------------------------------
 # Map-Reduce
 def map_reduce(  # noqa:C901
-    split: Callable,
-    apply: Callable,
-    combine: Callable,
-    *inp: Union[Artefact, str, bytes],
-    noutputs: int = 1,
+    split: Callable[[Dict[str, bytes]], Sequence[Dict[str, bytes]]],
+    apply: Callable[[Dict[str, Artefact]], Dict[str, Artefact]],
+    combine: Callable[[list[Dict[str, Artefact]]], Dict[str, Artefact]],
+    out: _OUT_FILES,
+    inp: _INP_FILES = None,
     name: Optional[str] = None,
     strict: bool = True,
     opt: Optional[Options] = None,
     connection: Optional[Redis[bytes]] = None,
-) -> tuple[Artefact, ...]:
+) -> Operation:
     """Perform a map-reduce using three functions."""
     opt = get_options(opt)
     db = get_db(connection)
-    arg_names = []
-    inputs = {}
-    for k, arg in enumerate(inp):
-        arg_names += [f"in{k}"]
-        if isinstance(arg, Artefact):
-            inputs[arg_names[-1]] = arg
-        else:
-            inputs[arg_names[-1]] = put(arg, connection=db)
 
-    # output slots
-    outputs = [f"out{k}" for k in range(noutputs)]
+    inputs: dict[str, Artefact] = {}
+    # Parse input files -------------------------------------
+    if inp is None:
+        pass
+    # multiple input files as a mapping
+    elif isinstance(inp, Mapping):
+        for key, val in inp.items():
+            skey = str(key)
+            if isinstance(val, Artefact):
+                inputs[skey] = val
+            else:
+                inputs[skey] = put(val, connection=db)
+    else:
+        raise TypeError(f"{inp} not a valid file input")
+
+    outputs = [str(o) for o in out]
 
     if name is not None:
         fun_name = name
@@ -84,18 +92,20 @@ def map_reduce(  # noqa:C901
             + "_{combine.__qualname__}:{noutputs}"
         )
 
-    def map_reduce(inpd: dict[str, bytes]) -> dict[str, _graph.Artefact]:
+    def map_reduce(inpd: dict[str, bytes]) -> dict[str, Artefact]:
         """Perform the map reduce."""
-        args = [inpd[key] for key in arg_names]
-        inp_data = split(args)
-        out_artefacts = []
-        for el in inp_data:
-            artefact = put(el)
-            out_artefacts += [apply(artefact)]
-        return {"out": combine(out_artefacts)}
+        split_data = split(inpd)
+        out_data = []
+        for onedict in split_data:
+            inp2 = {}
+            for key, val in onedict.items():
+                inp2[key] = put(val)
+            out_data += [apply(inp2)]
+        return combine(out_data)
 
     # Generate the subdag operations
-    cmd = subdag_funsie(map_reduce, arg_names, ["out"], name=fun_name, strict=strict)
-    operation = make_op(db, cmd, inp, opt)
-    out = Artefact.grab(db, operation.out["out"])
-    return out
+    cmd = subdag_funsie(
+        map_reduce, list(inputs.keys()), outputs, name=fun_name, strict=True
+    )
+    operation = make_op(db, cmd, inputs, opt)
+    return operation
---
+++ tests/test_dynamic.py
@@ -0,0 +1,100 @@
+"""Tests of dynamic funsies."""
+from __future__ import annotations
+
+# external
+from fakeredis import FakeStrictRedis as Redis
+
+# module
+import funsies
+from funsies import dynamic
+from funsies.types import Artefact
+
+
+def test_map_reduce() -> None:
+    """Test simple map-reduce."""
+
+    def split(inp: dict[str, bytes]) -> list[dict[str, bytes]]:
+        a = inp["a"].split()
+        b = inp["b"].split()
+        out = []
+        for ia, ib in zip(a, b):
+            out += [
+                {
+                    "sum": str(int(ia.decode()) + int(ib.decode())).encode(),
+                    "product": str(int(ia.decode()) * int(ib.decode())).encode(),
+                }
+            ]
+        return out
+
+    def apply(inp: dict[str, Artefact]) -> dict[str, Artefact]:
+        return {"cat": funsies.utils.concat(inp["sum"], inp["product"], join="//")}
+
+    def combine(inp: list[dict[str, Artefact]]) -> dict[str, Artefact]:
+        return {"out": funsies.utils.concat(*[el["cat"] for el in inp], join="\n")}
+
+    with funsies.Fun(Redis(), funsies.options(distributed=False)) as db:
+        num1 = funsies.put("1 2 3 4 5")
+        num2 = funsies.put("11 10 11 10 11")
+
+        operation = dynamic.map_reduce(
+            split, apply, combine, inp={"a": num1, "b": num2}, out=["out"]
+        )
+        g = Artefact.grab(db, operation.out["out"])
+        final = funsies.morph(lambda x: x, g)
+        funsies.execute(final)
+        print(final.hash)
+
+
+def test_nested_map_reduce(nworkers:int) -> None:
+    """Test nested map-reduce."""
+
+    def sum_inputs(*inp: bytes) -> bytes:
+        out = 0
+        for el in inp:
+            out += int(el.decode())
+        return str(out).encode()
+
+    def split2(inp: dict[str, bytes]) -> list[dict[str, bytes]]:
+        a = inp["in"].split(b" ")
+        out = [{"out": el} for el in a]
+        return out
+
+    def apply2(inp: dict[str, Artefact]) -> dict[str, Artefact]:
+        return {"out": funsies.reduce(sum_inputs, inp['out'], '1')}
+
+    def combine2(inp: list[dict[str, Artefact]]) -> dict[str, Artefact]:
+        return {"out": funsies.reduce(sum_inputs, *[el["out"] for el in inp])}
+
+    def split(inp: dict[str, bytes]) -> list[dict[str, bytes]]:
+        a = inp["in"].split(b"\n")
+        out = [{"out": el} for el in a]
+        return out
+
+    def apply(inp: dict[str, Artefact]) -> dict[str, Artefact]:
+        operation = dynamic.map_reduce(
+            split2, apply2, combine2, inp={"in": inp["out"]}, out=["out"]
+        )
+        return {"out": Artefact.grab(funsies._context.get_db(), operation.out["out"])}
+
+    def combine(inp: list[dict[str, Artefact]]) -> dict[str, Artefact]:
+        return {"out": funsies.utils.concat(*[el["out"] for el in inp], join=",,")}
+
+    with funsies.ManagedFun(nworkers=nworkers) as db:
+        num = funsies.put("1 2\n3 4 7\n10 12\n1")
+        # split -> 1 2\n 3 4 7\n10 12\n1
+        # apply -> split2 -> 1, 2 | 3,4,7|10,12|1
+        # apply2 -> 2, 3| 4,5,8|11,13|2
+        # combine2 -> 5|17|24|2
+        # combine -> 5,,17,,24,,2
+
+        operation = dynamic.map_reduce(
+            split, apply, combine, inp={"in": num}, out=["out"]
+        )
+        g = Artefact.grab(db, operation.out["out"])
+        final = funsies.morph(lambda x: x, g)
+        funsies.execute(final)
+        funsies.wait_for(final)
+        assert funsies.take(final) == b'5,,17,,24,,2'
+
+
+test_nested_map_reduce(1)
