8bbde95d0c01b9bd5335f3e69d62fa8734dd8fc0
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-03-01 16:52:18 -0500

    popped version to 0.6 to reflect massive changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="funsies",
-    version="0.4.0",
+    version="0.6.0",
     author="Cyrille Lavigne",
     author_email="cyrille.lavigne@mail.utoronto.ca",
     description="Funsies is an opinionated typed python library to build"
