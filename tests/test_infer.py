63ae89239d1bb309c79ebd615d2a6e8e369dc6cf
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-04-02 16:55:16 -0400

    tested/linted

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -2,7 +2,7 @@
 # from __future__ import annotations
 
 # std
-from typing import Any, Dict, Tuple, Sequence, List
+from typing import Dict, Tuple
 
 # external
 from fakeredis import FakeStrictRedis as Redis
@@ -39,9 +39,6 @@ def test_infer() -> None:
     def test_fun6(a: int, b: int) -> Tuple[int, ...]:
         ...
 
-    def test_fun7(a: 'Thing', b: int) -> float:  # noqa
-        ...
-
     assert _infer.output_types(test_fun) == (types.Encoding.blob,)
     with pytest.raises(TypeError):
         assert _infer.output_types(test_fun4) is None
@@ -56,7 +53,18 @@ def test_infer() -> None:
     with pytest.raises(TypeError):
         assert _infer.output_types(test_fun6) is None
 
-    assert _infer.output_types(test_fun7) is None
+
+def test_infer_forward_ref() -> None:
+    """Test inference of unkown forward refs."""
+
+    def test_fun7(a: "Thing", b: int) -> float:  # type:ignore # noqa
+        ...
+
+    def test_fun7b(a: "Thing", b: int) -> Dict[float, "what"]:  # type:ignore # noqa
+        ...
+
+    assert _infer.output_types(test_fun7) == (types.Encoding.json,)
+    assert _infer.output_types(test_fun7b) == (types.Encoding.json,)
 
 
 def test_infer_errs() -> None:
@@ -95,4 +103,3 @@ def test_infer_errs() -> None:
         # If we pass out= then the inference is skipped
         out = f.morph(i1o2, a, out=types.Encoding.blob)  # type:ignore # noqa:F841
         out = f.reduce(i1o2, a, out=types.Encoding.blob)  # type:ignore # noqa:F841
-
