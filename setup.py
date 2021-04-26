2872094faf0532d0a816f4d02215ee3536265c0c
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-04-26 11:25:06 -0400

    prepping for release

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 """Install funsies."""
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="funsies",
-    version="0.6.0",
+    version="0.7.0",
     author="Cyrille Lavigne",
     author_email="cyrille.lavigne@mail.utoronto.ca",
-    description="Funsies is an opinionated typed python library to build"
-    + " reproducible, composable and data-persistent computational workflows "
-    + "that are described entirely in Python.",
+    description="Funsies is a library to build and exectution engine for"
+    + " reproducible, composable and data-persistent computational workflows.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/aspuru-guzik-group/funsies",
     package_dir={"": "src"},
     package_data={"funsies": ["py.typed"]},  # mypy exports
@@ -29,4 +35,20 @@ setuptools.setup(
         [console_scripts]
         funsies=funsies._cli:main
     """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        #
+        "Typing :: Typed",
+        #
+        "License :: OSI Approved :: MIT License",
+        #
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        #
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    keywords="workflows hashtree redis compchem chemistry parallel hpc",
 )
